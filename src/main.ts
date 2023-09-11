2c14cfa9ccb3b846b1cbd8c6e61a494c7d30d66b
Turnip <2276157075@qq.com>
2023-09-11 21:41:49 +0800

    chore(all): 初始化
    初始化这个项目

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import { createApp } from "vue";
-import ArcoVue from "@arco-design/web-vue";
-import App from "./App.vue";
-import "@arco-design/web-vue/dist/arco.css";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-const app = createApp(App);
-app.use(ArcoVue);
-app.mount("#app");
+createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,62 @@
-import path from "node:path";
+import path from "path";
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import Vue from "@vitejs/plugin-vue";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
-import { ArcoResolver } from "unplugin-vue-components/resolvers";
-import { vitePluginForArco } from "@arco-plugins/vite-vue";
-// https://vitejs.dev/config/
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Inspect from "vite-plugin-inspect";
+
+const pathSrc = path.join(__dirname, "src");
+
 export default defineConfig({
-  plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": pathSrc,
     },
   },
+  plugins: [
+    Vue(),
+    AutoImport({
+      // Auto import functions from Vue, e.g. ref, reactive, toRef...
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ["vue"],
+
+      // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [
+        ElementPlusResolver(),
+
+        // Auto import icon components
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: "Icon",
+        }),
+      ],
+
+      dts: path.resolve(pathSrc, "auto-imports.d.ts"),
+    }),
+
+    Components({
+      resolvers: [
+        // Auto register icon components
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ["ep"],
+        }),
+        // Auto register Element Plus components
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+      ],
+
+      dts: path.resolve(pathSrc, "components.d.ts"),
+    }),
+
+    Icons({
+      autoInstall: true,
+    }),
+
+    Inspect(),
+  ],
 });
