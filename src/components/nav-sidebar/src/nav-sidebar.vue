b42c4ec9d59a30749d686793a7d0640adc88d676
Turnip <2276157075@qq.com>
2023-09-11 22:33:15 +0800

    chore: 初始化

--- src/components/nav-sidebar/src/nav-sidebar.vue
+++ src/components/nav-sidebar/src/nav-sidebar.vue
@@ -1,8 +1,5 @@
 <script lang="ts" setup>
 import NavAvatar from "./nav-avatar.vue";
-
-import { ref } from "vue";
-const active = ref(0);
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import path from "path";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 const pathSrc = path.join(__dirname, "src");
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      dts: path.resolve(pathSrc, "auto-imports.d.ts"),
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dts: path.resolve(pathSrc, "auto-imports.d.ts"),
+    }),
+  ],
   resolve: {
     alias: {
       "@": pathSrc,
